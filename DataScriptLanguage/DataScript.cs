c7a845a5984e00e042dbd6eed6b0b9423b74f65d
Unknown <marc.cookerussell@gmail.com>
2018-12-30 23:00:18 +1100

    Data grouping

--- DataScriptLanguage/DataScript.cs
+++ DataScriptLanguage/DataScript.cs
@@ -18,23 +18,31 @@ namespace DataScriptLanguage
         {
             string[] lines = File.ReadAllText(path).Replace("\r", "").Split('\n');
 
-            foreach (string s in lines)
+            List<string> group = new List<string>();
+            foreach (string line in lines)
             {
+                string s = line.Trim();
                 if (!s.StartsWith("//") && s.Length > 0)
                 {
-
-                    foreach (DataItem item in Items)
+                    if (!s.Contains(":") && !s.Contains("}"))
+                        group.Add(s.Split(new char[] { ' ' }, 2)[0]);
+                    else if (s == "}")
+                        group.RemoveAt(group.Count - 1);
+                    else
                     {
-                        string name = s.Split(new char[] { ':' }, 2)[0];
-                        if (item.GetName() == name)
+                        foreach (DataItem item in Items)
                         {
-                            string d = s.Split(new char[] { ':' }, 2)[1];
-                            if (d.StartsWith(" "))
-                                d = d.Substring(1, d.Length - 1);
-                            string[] data = Regex.Matches(d, @"[\""].+?[\""]|[^,]+")
-                                .Cast<Match>()
-                                .Select(m => m.Value.Replace("\"", "")).ToArray();
-                            item.SetData(data);
+                            string name = string.Join(".", group) + (group.Count > 0 ? "." : "") + s.Split(new char[] { ':' }, 2)[0];
+                            if (item.GetName() == name)
+                            {
+                                string d = s.Split(new char[] { ':' }, 2)[1];
+                                if (d.StartsWith(" "))
+                                    d = d.Substring(1, d.Length - 1);
+                                string[] data = Regex.Matches(d, @"[\""].+?[\""]|[^,]+")
+                                    .Cast<Match>()
+                                    .Select(m => m.Value.Replace("\"", "")).ToArray();
+                                item.SetData(data);
+                            }
                         }
                     }
                 }
